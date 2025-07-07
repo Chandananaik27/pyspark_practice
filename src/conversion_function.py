d14eda6d17cffbd9bf57f568950b1ae2e5258e94
Your Full Name <your@email.com>
2025-07-07 15:34:27 +0530

    datatype conversion functions.

--- src/conversion_function.py
+++ src/conversion_function.py
@@ -9,15 +9,15 @@ data=[("James",34,"2006-01-01","true","M",3000.60),
 
 columns = ["firstname","age","jobStartDate","isGraduated","gender","salary"]
 df= spark.createDataFrame(data ,columns)
-# df.printSchema()
-# df.show()
-# df2=df.withColumn("age",df.age.cast("string"))\
-#       .withColumn("firstname",col("firstname").cast(IntegerType()))
-# print("withcolumn")
-# df2.printSchema()
-# df3=df.select(col("isGraduated").cast("boolean"))
-# print("select")
-# df3.printSchema()
+df.printSchema()
+df.show()
+df2=df.withColumn("age",df.age.cast("string"))\
+      .withColumn("firstname",col("firstname").cast(IntegerType()))
+print("withcolumn")
+df2.printSchema()
+df3=df.select(col("isGraduated").cast("boolean"))
+print("select")
+df3.printSchema()
 df.createOrReplaceTempView("CastExample")
 df4 = spark.sql("SELECT STRING(age),BOOLEAN(isGraduated),DATE(jobStartDate) from CastExample")
 df4.printSchema()
